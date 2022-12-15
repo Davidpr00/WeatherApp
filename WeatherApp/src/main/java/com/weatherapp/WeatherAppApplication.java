731d173f3643b62749987bd7ec4b3ed30eb1f9cc
Davidpr00 <david.praslicka@gmail.com>
2022-12-15 11:10:06 +0100

    NEW FEATURE() - CommandLineRunner to create user if no users present

--- WeatherApp/src/main/java/com/weatherapp/WeatherAppApplication.java
+++ WeatherApp/src/main/java/com/weatherapp/WeatherAppApplication.java
@@ -1,13 +1,30 @@
 package com.weatherapp;
 
+import com.weatherapp.users.models.Account;
+import com.weatherapp.users.models.RoleEnum;
+import com.weatherapp.users.repositories.AccountRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeatherAppApplication {
+public class WeatherAppApplication implements CommandLineRunner {
+  private final AccountRepository accountRepository;
+
+  public WeatherAppApplication(AccountRepository accountRepository) {
+    this.accountRepository = accountRepository;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(WeatherAppApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+    if (accountRepository.findAll().size() < 1) {
+      Account account = new Account("david", "123456789", "david.praslicka@gmail.com");
+      account.setRole(RoleEnum.USER);
+      accountRepository.save(account);
+    }
+  }
 }
